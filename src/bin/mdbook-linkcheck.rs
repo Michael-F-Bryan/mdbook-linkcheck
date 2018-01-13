1df6a9c1157c63884c0181dff243d30f0bc1c75b
Michael Bryan <michaelfbryan@gmail.com>
2018-01-13 18:10:17 +0800

    Copied across the boilerplate from mdbook-epub

--- src/bin/mdbook-linkcheck.rs
+++ src/bin/mdbook-linkcheck.rs
@@ -1,7 +1,70 @@
-extern crate mdbook_linkcheck;
+extern crate env_logger;
+extern crate failure;
 extern crate mdbook;
+extern crate mdbook_linkcheck;
+extern crate pulldown_cmark;
 extern crate serde_json;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use std::io;
+use std::env;
+use std::process;
+use std::path::PathBuf;
+use failure::{Error, ResultExt, SyncFailure};
+use structopt::StructOpt;
+use mdbook::renderer::RenderContext;
+use mdbook::MDBook;
 
 fn main() {
-    unimplemented!();
+    env_logger::init();
+    let args = Args::from_args();
+
+    if let Err(e) = run(&args) {
+        eprintln!("Error: {}", e);
+
+        for cause in e.causes().skip(1) {
+            eprintln!("\tCaused By: {}", cause);
+        }
+
+        if let Ok(_) = env::var("RUST_BACKTRACE") {
+            eprintln!();
+            eprintln!("{}", e.backtrace());
+        }
+
+        process::exit(1);
+    }
+}
+
+fn run(args: &Args) -> Result<(), Error> {
+    // get a `RenderContext`, either from stdin (because we're used as a plugin)
+    // or by instrumenting MDBook directly (in standalone mode).
+    let ctx: RenderContext = if args.standalone {
+        let md = MDBook::load(&args.root).map_err(SyncFailure::new)?;
+        let destination = md.build_dir_for("epub");
+
+        RenderContext {
+            version: mdbook_linkcheck::MDBOOK_VERSION.to_string(),
+            root: md.root,
+            book: md.book,
+            config: md.config,
+            destination: destination,
+        }
+    } else {
+        serde_json::from_reader(io::stdin()).context("Unable to parse RenderContext")?
+    };
+
+    mdbook_linkcheck::check_links(&ctx)?;
+
+    Ok(())
+}
+
+#[derive(Debug, Clone, StructOpt)]
+struct Args {
+    #[structopt(short = "s", long = "standalone",
+                help = "Run standalone (i.e. not as a mdbook plugin)")]
+    standalone: bool,
+    #[structopt(help = "The book to render.", parse(from_os_str), default_value = ".")]
+    root: PathBuf,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+//! A `mdbook` backend which will check all links in a document are valid.
+
 extern crate mdbook;
+#[macro_use]
+extern crate log;
 #[macro_use]
 extern crate failure;
 extern crate serde;
@@ -11,6 +15,27 @@ extern crate pulldown_cmark;
 use failure::Error;
 use mdbook::renderer::RenderContext;
 
+/// The exact version of `mdbook` this crate is compiled against.
+pub const MDBOOK_VERSION: &'static str = env!("MDBOOK_VERSION");
+
+/// The main entrypoint for this crate.
 pub fn check_links(ctx: &RenderContext) -> Result<(), Error> {
+    info!("Checking for broken links");
+
     unimplemented!()
+}
+
+
+/// A collection of broken links.
+#[derive(Debug, Clone, PartialEq, Fail)]
+#[fail(display = "several broken links were found")]
+pub struct BrokenLinks {
+    pub links: Vec<BrokenLink>,
+}
+
+/// A broken link.
+#[derive(Debug, Clone, PartialEq)]
+pub struct BrokenLink {
+    pub url: String,
+    pub chapter: String,
 }
